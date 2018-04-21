cbdd2e6b4f38b5a6360da7cb79d6d06b4d7b215c
rodrigovallades <rodrigo.vallades@gmail.com>
2018-04-20 23:41:48 -0300

    Improve readability

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -6,40 +6,39 @@ import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } fr
 
 chai.use(sinonChai);
 sinonStubPromise(sinon);
-
 global.fetch = require('node-fetch');
 
-describe('Spotify wrapper', () => {
-  let fetchedStub;
+describe('Search', () => {
+  let stubedFetch;
   let promise;
 
   beforeEach(() => {
-    fetchedStub = sinon.stub(global, 'fetch');
-    promise = fetchedStub.returnsPromise();
+    stubedFetch = sinon.stub(global, 'fetch');
+    promise = stubedFetch.returnsPromise();
   });
 
   afterEach(() => {
-    fetchedStub.restore();
+    stubedFetch.restore();
   });
 
   describe('Smoke tests', () => {
-    it('should exist the search method', () => {
+    it('should exist a search method', () => {
       expect(search).to.exist;
     });
 
-    it('should exist the searchAlbums method', () => {
+    it('should exist a searchAlbums method', () => {
       expect(searchAlbums).to.exist;
     });
 
-    it('should exist the searchArtists method', () => {
+    it('should exist a searchArtists method', () => {
       expect(searchArtists).to.exist;
     });
 
-    it('should exist the searchTracks method', () => {
+    it('should exist a searchTracks method', () => {
       expect(searchTracks).to.exist;
     });
 
-    it('should exist the searchPlaylists method', () => {
+    it('should exist a searchPlaylists method', () => {
       expect(searchPlaylists).to.exist;
     });
   });
@@ -47,29 +46,29 @@ describe('Spotify wrapper', () => {
   describe('Generic search', () => {
     it('should call fetch', () => {
       const artist = search();
-      expect(fetchedStub).to.have.been
+      expect(stubedFetch).to.have.been
         .calledOnce;
     });
 
     it('should fetch the correct endpoint', () => {
       context('passing one type', () => {
         const artist = search('Queen', 'artist');
-        expect(fetchedStub).to.have.been
+        expect(stubedFetch).to.have.been
           .calledWith('https://api.spotify.com/v1/search?q=Queen&type=artist');
 
         const album = search('Queen', 'album');
-        expect(fetchedStub).to.have.been
+        expect(stubedFetch).to.have.been
           .calledWith('https://api.spotify.com/v1/search?q=Queen&type=album');
       });
 
       context('passing more than one type', () => {
         const artist = search('Queen', ['artist', 'album']);
-        expect(fetchedStub).to.have.been
+        expect(stubedFetch).to.have.been
           .calledWith('https://api.spotify.com/v1/search?q=Queen&type=artist,album');
       });
     });
 
-    it('should return JSON data from promise', () => {
+    it('should receive correct response from Promise', () => {
       promise.resolves({ body: 'json' });
       const artist = search('Queen', 'artist');
       expect(artist.resolveValue).to.be
@@ -80,13 +79,13 @@ describe('Spotify wrapper', () => {
   describe('searchAlbums', () => {
     it('should call fetch', () => {
       const albums = searchAlbums('Queen');
-      expect(fetchedStub).to.have.been
+      expect(stubedFetch).to.have.been
         .calledOnce;
     });
 
     it('should fetch albums on the endpoint', () => {
       const albums = searchAlbums('Queen');
-      expect(fetchedStub).to.have.been
+      expect(stubedFetch).to.have.been
         .calledWith('https://api.spotify.com/v1/search?q=Queen&type=album');
     });
   });
@@ -94,13 +93,13 @@ describe('Spotify wrapper', () => {
   describe('searchArtists', () => {
     it('should call fetch', () => {
       const artist = searchArtists('Queen');
-      expect(fetchedStub).to.have.been
+      expect(stubedFetch).to.have.been
         .calledOnce;
     });
 
     it('should fetch artist on the endpoint', () => {
       const artist = searchArtists('Queen');
-      expect(fetchedStub).to.have.been
+      expect(stubedFetch).to.have.been
         .calledWith('https://api.spotify.com/v1/search?q=Queen&type=artist');
     });
   });
@@ -108,13 +107,13 @@ describe('Spotify wrapper', () => {
   describe('searchTracks', () => {
     it('should call fetch', () => {
       const tracks = searchTracks('Queen');
-      expect(fetchedStub).to.have.been
+      expect(stubedFetch).to.have.been
         .calledOnce;
     });
 
     it('should fetch tracks on the endpoint', () => {
       const tracks = searchTracks('Queen');
-      expect(fetchedStub).to.have.been
+      expect(stubedFetch).to.have.been
         .calledWith('https://api.spotify.com/v1/search?q=Queen&type=track');
     });
   });
@@ -122,13 +121,13 @@ describe('Spotify wrapper', () => {
   describe('searchPlaylists', () => {
     it('should call fetch', () => {
       const playlists = searchPlaylists('Queen');
-      expect(fetchedStub).to.have.been
+      expect(stubedFetch).to.have.been
         .calledOnce;
     });
 
     it('should fetch playlists on the endpoint', () => {
       const playlists = searchPlaylists('Queen');
-      expect(fetchedStub).to.have.been
+      expect(stubedFetch).to.have.been
         .calledWith('https://api.spotify.com/v1/search?q=Queen&type=playlist');
     });
   });
